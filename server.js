f8ec652f8035a753a7e2330424864d3cbb6e7707
Lazarus Chenana <ltchenana1405@gmail.com>
2026-04-29 14:36:14 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(express.json());
+
+const SUPABASE_URL = "https://ttnllyjrjgdyllylpjts.supabase.co";
+const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bmxseWpyamdkeWxseWxwanRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1Mjk1ODEsImV4cCI6MjA5MjEwNTU4MX0.48N---TSkIFiOcqUGqvY_NV7vGjqpMivPCvGPzYM0XQ
+
+// TEST ROUTE
+app.get("/", (req, res) => {
+  res.send("Gridv21 API running");
+});
+
+// GET TOOLS
+app.get("/tools", async (req, res) => {
+  const response = await fetch(`${SUPABASE_URL}/rest/v1/tools`, {
+    headers: {
+      "apikey": SUPABASE_KEY,
+      "Authorization": `Bearer ${SUPABASE_KEY}`
+    }
+  });
+
+  const data = await response.json();
+  res.json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
